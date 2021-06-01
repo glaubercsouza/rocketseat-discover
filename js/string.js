b082de3d8b89cdd442af0f3672e061a63aec1338
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-05-31 21:48:18 -0300

    Create string.js

---
+++ js/string.js
@@ -0,0 +1,13 @@
+/*
+  String
+
+  * Cadeia de caracteres
+
+  "" // aspas duplas
+  '' // aspas simples
+  `` // template literals ou template strings
+*/
+
+console.log('Glauber')
+console.log("Glauber")
+console.log (`Glauber ${1 + 1}`)
