1a79aa58180d164b1f4c19e113e423a5c61643fc
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-05-31 21:55:17 -0300

    Create number.js

---
+++ js/number.js
@@ -0,0 +1,14 @@
+/* 
+Number
+  * números
+
+  33 // inteiros
+  12.5 // reais - float
+  NaN // Not a Number
+  Infinity // infinito
+*/
+
+console.log(33) //inteiro
+console.log(12.5) // real
+console.log(12.5 / "asdf") // NaN
+console.log(Infinity) // infinito
