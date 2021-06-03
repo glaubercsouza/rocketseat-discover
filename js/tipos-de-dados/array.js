7aa5751ec38e6c6842e793f538e7eeb488270447
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-03 11:43:47 -0300

    Create array.js

---
+++ js/tipos-de-dados/array.js
@@ -0,0 +1,16 @@
+/*
+  * Array (Vetores)
+    * Uma lista
+    * Agrupamento de dado
+    
+    ["Glauber", 28]
+*/
+
+console.log(["Glauber", 28])
+
+console.log([
+  "Leite",
+  "Ovos",
+  2,
+  3
+])
