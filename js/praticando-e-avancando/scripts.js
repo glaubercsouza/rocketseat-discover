ac7ed1d546fb0e2c021d772a513b4ed95a4c86bb
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-20 11:27:58 -0300

    Update scripts.js

--- js/praticando-e-avancando/scripts.js
+++ js/praticando-e-avancando/scripts.js
@@ -8,3 +8,15 @@ const person = {
 }
 
 console.log(`${person.name} tem ${person.age} anos`)
+
+// Array
+
+const animals = [
+  'Lion',
+  'Monkey',
+  'Cat'
+]
+
+// acessar valores dentro do Array
+console.log(animals[0])
+ 
