4f8d2fd5f664a2129f54851533749098356809b0
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-21 21:28:10 -0300

    ex01

---
+++ js/praticando-e-avancando/ex01.js
@@ -0,0 +1,47 @@
+// 1.
+let weight = '79';
+
+// 2.
+console.log(typeof weight);
+
+// 3.
+let name = 'Glauber';
+let age = 28;
+let stars = 4.9;
+let isSubscribed = true;
+
+// 4
+let student = {
+  name: 'Glauber',
+  age: 28,
+  weight: 78.5,
+  isSubscribed: true,
+};
+console.log(typeof student);
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`);
+
+// 5
+let students = []
+
+// 6
+students = [
+  student
+]
+
+// 7
+console.log(students[0])
+
+// 8
+const costa = {
+  name: 'Costa',
+  age: 27,
+  weight: 50,
+  isSubscribed: false,
+}
+
+student[1] = costa;
+
+// 9 
+console.log(a)
+var a = 1
