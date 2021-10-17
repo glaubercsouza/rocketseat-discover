a6e0049bd9280bcf9f9a9dab4a1ced2a1b8be88c
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-10-17 16:39:42 -0300

    functions

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,16 +1,9 @@
-// function expression
-// function anonymous
+// Função é um liquidificador
 
-// parâmetros (parameters)
-const sum = function(number1, number2){
-  total = number1 + number2
-  return total
+function fazerSuco(fruta1, fruta2) {
+  return fruta1 + fruta2
 }
 
+const copo = fazerSuco('banana', 'maçã')
 
-let number1 = 34
-let number2 = 25
-
-console.log(`o número 1 é ${number1}`)
-console.log(`o número 2 é ${number2}`)
-console.log(`a soma é ${sum(number1, number2)}`)
+console.log(copo)
